3cf9e8f03933f2f7e07384beadd29ddb4a5f02a0
donald <donald.j.mclaughlin@gmail.com>
2025-07-08 22:04:29 -0400

    fix enemy orientation, enemy chase npc done

--- features/enemies/drone/drone.gd
+++ features/enemies/drone/drone.gd
@@ -2,8 +2,9 @@ extends CharacterBody3D
 # In your Drone's script (e.g., drone.gd)
 
 @export var patrol_path: NodePath
-@export var movement_speed = 10.0
-@export var rotation_speed = 3.0
+@export var movement_speed = 5.0
+
+@export var fire_rate = 5.0
 var path_follow: PathFollow3D
 @onready var nav_agent = $pivot/NavigationAgent3D
 @onready var detection_area = $pivot/DetectionArea
@@ -13,11 +14,11 @@ var path_follow: PathFollow3D
 
 # How fast the drone moves along the path
 var patrol_speed = 5.0
-
+var cooldown: float
 enum States {IDLE, PATROL, ALERT, PURSUE, FIRE}
 var current_state = States.IDLE
 var target = null
-var scan_speed = 1.0
+var scan_speed = 2.0
 func _ready():
 	# Get the PathFollow3D node from the path you assign in the Inspector
 	if not patrol_path.is_empty():
@@ -27,7 +28,8 @@ func _ready():
 	detection_area.body_exited.connect(_on_player_exited)
 
 func _physics_process(delta):
-
+	if cooldown > 0:
+			cooldown = cooldown - delta
 	match current_state:
 		States.IDLE:
 			current_state = States.PATROL
@@ -59,6 +61,8 @@ func _physics_process(delta):
 			# else States.idle
 			if is_instance_valid(target):
 				print("I AM PURSUING")
+				look_at(target.global_position, Vector3.UP)
+				
 				var next_point = nav_agent.get_next_path_position()
 				var direction = (next_point - global_position).normalized()
 				nav_agent.target_position = target.global_position
@@ -67,10 +71,25 @@ func _physics_process(delta):
 				velocity = direction * movement_speed
 				move_and_slide()
 				
+				if barrel.is_colliding():
+					
+					var what_i_hit = barrel.get_collider()
+					if is_instance_valid(what_i_hit) and what_i_hit.has_method("take_damage"):
+						current_state = States.FIRE
+					else:
+						current_state = States.PURSUE
+				else: 
+					current_state = States.PURSUE
 				
 		States.FIRE:
 			if is_instance_valid(target):
-				pass
+				if cooldown <= 0:
+					cooldown = 1/fire_rate
+					print("FIRING")
+					target.take_damage(1)
+					current_state = States.PURSUE
+				else:
+					current_state = States.PURSUE
 
 
 func follow_path(delta) -> void:
